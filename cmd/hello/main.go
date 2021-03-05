df7d97fb89a7bbfe183fb1c3c7e102a349aba69f
azarai <azarai@codeboje.de>
2021-03-05 17:16:22 +0100

    Update main.go

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
 	"microappsdev.com/go-test/internal/pkg/utils"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	fmt.Print("Got request for '/.netlify/functions/goodbye', this message is dumpled by 'cmd/goodbye/main.go'")
+	lc, ok := lambdacontext.FromContext(ctx)
+	if !ok {
+		return &events.APIGatewayProxyResponse{
+			StatusCode: 503,
+			Body:       "Something went wrong :(",
+		}, nil
+	}
+
+	cc := lc.ClientContext
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       utils.IntroduceYourself("Hello, I'm from 'cmd/goodbye/main.go'!"),
+		Body:       utils.IntroduceYourself(cc.Client.AppTitle),
 	}, nil
 }
 
