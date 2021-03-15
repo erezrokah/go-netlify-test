0d6abb32cf1558625047a4a456721211bf04a60a
erezrokah <erezrokah@users.noreply.github.com>
2021-03-15 15:23:58 +0100

    chore: reproduce issue

--- cmd/goodbye/main.go
+++ cmd/goodbye/main.go
@@ -9,6 +9,7 @@ import (
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	fmt.Print("Got request for '/.netlify/functions/hello', this message is dumpled by 'cmd/goodbye/main.go'")
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, I'm from 'cmd/goodbye/main.go'!",
